39137834ea97f2e0421677c7fdd5e87f8b7ab8e5
rnascunha85 <rnascunha@gmail.com>
2021-04-20 14:08:55 -0300

    MSVC printf type fix

--- src/include/tt/port.hpp
+++ src/include/tt/port.hpp
@@ -7,7 +7,11 @@
 namespace Tree_Trunks{
 
 using time_t = std::time_t;
+#if defined(_MSC_VER)
+static constexpr const char* time_str = "%llu";
+#else /* _MSVC_VER */
 static constexpr const char* time_str = "%lu";
+#endif /* _MSVC_VER */
 auto const time_func = []{ return std::time(NULL); };
 
 }//Tree_Trunks
